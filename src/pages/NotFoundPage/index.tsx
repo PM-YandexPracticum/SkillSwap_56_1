55e506ab26931d89b0f1460423b5f6a7865cf7f8
Dima Shestopalov <youngpozner@yandex.ru>
2026-09-08 23:30:22 +0300

    fix: доработать страницу 404

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,35 +1,45 @@
+import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '@/shared/lib/constants'
 import { Button } from '../../shared/ui/button/Button'
-import { useNavigate } from 'react-router-dom'
-
-const errorImage = '/assets/icons/page-error-404.svg'
+import errorImage from '../../shared/assets/page-error-404.svg'
+import styles from './NotFoundPage.module.css'
 
 export default function NotFoundPage() {
   const navigate = useNavigate()
+
   const handleClickToHome = () => {
     navigate(ROUTES.HOME)
   }
+
   return (
-    <main className="flex-1 flex items-center justify-center">
-      <div className="flex flex-col max-w-md w-full gap-8">
-        <div className="flex flex-col items-center">
-          <img src={errorImage} alt="страница не найдена" className="mb-10" />
-          <h1 className="text-2xl">Страница не найдена</h1>
-          <p className="text-center leading-6">
-            К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже
+    <main className={styles.page}>
+      <div className={styles.content}>
+        <div className={styles.message}>
+          <img
+            src={errorImage}
+            alt="Страница не найдена"
+            className={styles.image}
+          />
+
+          <h1 className={styles.title}>Страница не найдена</h1>
+
+          <p className={styles.description}>
+            К сожалению, эта страница недоступна. Вернитесь на главную страницу
+            или попробуйте позже
           </p>
         </div>
-        <div className="flex gap-4 w-full">
+
+        <div className={styles.actions}>
           <Button
-            className="flex-1 text-base font-normal py-6"
-            variant={'outline'}
-            onClick={() => {}}
-          >
-            Сообщить об ошибке
-          </Button>
-          <Button className="flex-1 text-base font-normal py-6" onClick={handleClickToHome}>
-            На главную
-          </Button>
+            text="Сообщить об ошибке"
+            className={`${styles.button} ${styles.secondaryButton}`}
+          />
+
+          <Button
+            text="На главную"
+            className={`${styles.button} ${styles.primaryButton}`}
+            onClick={handleClickToHome}
+          />
         </div>
       </div>
     </main>
