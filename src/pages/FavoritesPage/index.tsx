306f393fb7bc16c915385658f354fb1c2f01778c
Olha <borisova.olga.stat@gmail.com>
2026-09-18 23:26:57 +0300

    feat: finish favorites page

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -1,23 +1,30 @@
-// TODO: реализовать страницу FavoritesPage
-import styles from '../FavoritesPage/FavoritesPage.module.css';
-import { AuthenticatedHeader } from '../../widgets/AuthenticatedHeader/AuthenticatedHeader';
-import { Footer } from '../../widgets/Footer/Footer';
-import requestsIcon from '../../shared/assets/requests.svg';
-import exchangesIcon from '../../shared/assets/exchanges.svg';
-import favoritesIcon from '../../shared/assets/favorites.svg';
-import skillsIcon from '../../shared/assets/skills.svg';
-import profileIcon from '../../shared/assets/profile.svg';
-import { SkillCard, SkillCardProps } from '../../entities/skill/ui/SkillCard';
-import { mock } from '../../entities/skill/ui/mock';
+import styles from '../FavoritesPage/FavoritesPage.module.css'
+import { AuthenticatedHeader } from '../../widgets/AuthenticatedHeader/AuthenticatedHeader'
+import { Footer } from '../../widgets/Footer/Footer'
+import requestsIcon from '../../shared/assets/requests.svg'
+import exchangesIcon from '../../shared/assets/exchanges.svg'
+import favoritesIcon from '../../shared/assets/favorites.svg'
+import skillsIcon from '../../shared/assets/skills.svg'
+import profileIcon from '../../shared/assets/profile.svg'
+import { SkillCard } from '../../entities/skill/ui/SkillCard'
+import { mock } from '../../entities/skill/ui/mock'
 
-const favorites = [{...mock, withButton: true}];
+const favorites = [
+  { ...mock, withButton: true },
+  { ...mock, withButton: true },
+  { ...mock, withButton: true },
+  { ...mock, withButton: true },
+  { ...mock, withButton: true },
+  { ...mock, withButton: true },
+  { ...mock, withButton: true },
+]
 
 export default function FavoritesPage() {
   return (
     <>
-    <AuthenticatedHeader />
-    <main className={styles.main}>
-      <aside className={styles.sidebar}>
+      <AuthenticatedHeader />
+      <main className={styles.main}>
+        <aside className={styles.sidebar}>
           <nav className={styles.navList}>
             <a href="#" className={styles.navItem}>
               <img src={requestsIcon} alt="Заявки" className={styles.sidebarIcon} />
@@ -27,7 +34,7 @@ export default function FavoritesPage() {
               <img src={exchangesIcon} alt="Мои обмены" className={styles.sidebarIcon} />
               <span>Мои обмены</span>
             </a>
-            <a href="#" className={styles.navItem}>
+            <a href="#" className={`${styles.navItem} ${styles.active}`}>
               <img src={favoritesIcon} alt="Избранное" className={styles.sidebarIcon} />
               <span>Избранное</span>
             </a>
@@ -42,14 +49,14 @@ export default function FavoritesPage() {
           </nav>
         </aside>
         <ul className={styles.cards}>
-        {favorites.map((card, index) => (
-          <li key={index}>
-            <SkillCard {...card} />
-          </li>
-        ))}
+          {favorites.map((card, index) => (
+            <li key={index}>
+              <SkillCard {...card} />
+            </li>
+          ))}
         </ul>
-    </main>
-    <Footer/>
+      </main>
+      <Footer />
     </>
   )
 }
