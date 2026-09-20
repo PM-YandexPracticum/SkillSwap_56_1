0ebd2c1fc4e66e04c48313522949a82db555fe61
Dogi <dogisocks@yandex.ru>
2026-09-20 14:11:13 +0300

    fix: searchableLayout

--- src/pages/CatalogPage/index.tsx
+++ src/pages/CatalogPage/index.tsx
@@ -1,12 +1,10 @@
+// TODO: реализовать страницу CatalogPage
 import { SearchableLayout } from '@/features/skill-search'
-import { Footer } from '@/widgets/Footer/Footer'
-import FiltersSidebar from '@/widgets/FiltersSidebar/FiltersSidebar'
-
 export default function CatalogPage() {
   return (
-    <>
-      <SearchableLayout sidebar={<FiltersSidebar />} />
-      <Footer />
-    </>
+    <main>
+      <SearchableLayout />
+    </main>
   )
 }
+
